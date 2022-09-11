26c08f053510a1905cee48d48515cab748bb9ea7
Abdallah Samy <abdallhsamy2011@gmail.com>
2022-09-11 11:58:26 +0200

    add instructions to homepage

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $url = route('search_owners_by_barcode');
+//Route::get('/', function () {
+//    $url = route('search_owners_by_barcode');
+//
+//    return sprintf('please use this url : <a href="%s" target="_blank">%s</a>', $url, $url);
+//});
 
-    return sprintf('please use this url : <a href="%s" target="_blank">%s</a>', $url, $url);
-});
+Route::view('/', 'instructions');
